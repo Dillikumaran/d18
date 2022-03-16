624f6358a75d28659f1bc679aea9b2d7316bfdfa
Dillikumaran <101106085+Dillikumaran@users.noreply.github.com>
2022-03-16 09:06:02 +0530

    Update task2day7.cs

--- task2day7.cs
+++ task2day7.cs
@@ -7,7 +7,7 @@ Console.WriteLine("Enter the number to be multiplied : ");
 int n= Convert.ToInt32(Console.ReadLine());
 for (int i = 1;i<=20;i++)
 {
-Console.WriteLine(n*i);
+Console.WriteLine(n+"*"+i+"="+(n*i));
 }
 }
 }
