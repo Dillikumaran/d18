21b07e65611f14e2f9479cbfc240d444504bb794
Dillikumaran <101106085+Dillikumaran@users.noreply.github.com>
2022-03-22 16:28:22 +0530

    Add files via upload

---
+++ day12task2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    [Serializable]
+    class day12task2
+    {
+        public String name = "Dilli";
+        public string gender = "male";
+        public int empId = 39290020;
+        public int age = 19;
+
+    }
+    class readwrite
+    {
+        public void infile()
+        {
+            day12task2 emp = new day12task2();
+            FileStream fs = new FileStream("Employee.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            BinaryFormatter f = new BinaryFormatter();
+            f.Serialize(fs, emp);
+            fs.Close();
+        }
+        public void outfile()
+        {
+            FileStream fs1 = new FileStream("Employee.txt", FileMode.Open, FileAccess.Read);
+            BinaryFormatter r = new BinaryFormatter();
+            day12task2 emp1 = (day12task2)r.Deserialize(fs1);
+            Console.WriteLine(emp1.empId);
+            Console.WriteLine(emp1.name);
+            Console.WriteLine(emp1.gender);
+            Console.WriteLine(emp1.age);
+            Console.ReadLine();
+        }
+        public static void Main()
+        {
+            readwrite sere = new readwrite();
+            sere.infile();
+            sere.outfile();
+        }
+    }
+}
+
---
+++ day21task1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections;
+
+namespace ConsoleApp1
+{
+    class day21task1
+    {
+        public static void Main()
+        {
+            ArrayList dessertlist = new ArrayList();
+            dessertlist.Add("kesari");
+            dessertlist.Add("laddu");
+            dessertlist.Add("jalebi");
+            dessertlist.Add("khewar");
+            FileStream fs = new FileStream("F:\\fstream\\task1.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.BaseStream.Seek(0, SeekOrigin.End);
+            foreach(string o in dessertlist)
+            sw.WriteLine(o);
+            sw.Flush();
+            fs.Close();
+            FileStream fs1 = new FileStream(@"f:\fstream\task1.txt", FileMode.Open, FileAccess.ReadWrite);
+            StreamReader sr = new StreamReader(fs1);
+            string outlist = sr.ReadToEnd();
+            Console.WriteLine(outlist);
+            fs1.Close();
+            FileInfo fi = new FileInfo(@"F:\fstream\task1.txt");
+            Console.WriteLine(fi.Length);
+            Console.WriteLine(fi.CreationTime);
+            Console.ReadLine();
+        }
+    }
+}
