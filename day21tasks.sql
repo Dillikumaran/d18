deaba2eda5013e75a79c9f8a0f9c15f3ec876e3f
Dillikumaran <101106085+Dillikumaran@users.noreply.github.com>
2022-04-04 23:44:10 +0530

    Update day21tasks.sql

--- day21tasks.sql
+++ day21tasks.sql
@@ -1,18 +1,18 @@
-﻿use AdventureWorks2019
+use AdventureWorks2019
 go
 --Task1
-select  COUNT(+[StandardCost]) Totalnoofrows
-from [Production].[ProductCostHistory]
+select  COUNT(+StandardCost) Totalnoofrows
+from Production.ProductCostHistory
 --USING SUM()
-select SUM([StandardCost]) SumCost
-from [Production].[ProductCostHistory]
+select SUM(StandardCost) SumCost
+from Production.ProductCostHistory
 --USING DATEDIFF()
 select DATEDIFF(YY,StartDate,EndDate) datedifference
-from [Production].[ProductCostHistory]
+from Production.ProductCostHistory
 --USING ADDDATE()
 SELECT DATEADD(year, 3, '2017/08/25') AS DateAdd
 --USING CONVERT()
-SELECT CONVERT(int,88.77);
+SELECT CONVERT(int,55.69);
 --USING LEFT()
 SELECT LEFT('Dilli', 3) AS ExtractString;
 --USING LEN()
@@ -22,40 +22,43 @@ select REVERSE('TeneT')
 
 --Task 2 
 
-create function fn_RupeesC(@amount as Nvarchar(10))
-returns Nvarchar(10)
+create function fn_RupeesC(@amount as Nvarchar(20))
+returns Nvarchar(20)
 as 
 begin
 return (N'₹'+@amount)
 end
-select BusinessEntityID,dbo.fn_RupeesC(rate) as Rupees 
+select BusinessEntityID,dbo.fn_RupeesC(rate) as Rupees
 from HumanResources.EmployeePayHistory
 
 --Task3
 
 create table tbl_student
 (
-Sno int primary key identity (1,1),
+Sno int primary key identity(1,1),
 StuName varchar(20),
 StuAge int,
 Gender char(1),
 MobileNO char(10)
-
 )
-insert into tbl_student values('Dilli',19,'M',9600584521)
-
+go
 create view task3
 as
-select Sno,StuName,StuAge
+(
+select Sno,StuName,StuAge,Gender,MobileNO
 from dbo.tbl_student
+)
 go
-select *from task3
-insert into task3 values('selena',19,'F',9945215421)
+insert into task3 values('selena',18,'F',9945213662)
+select * from tbl_student
+insert into tbl_student values('dilli',19,'M',9677052314)
+select * from task3
 
 --Task4
 
 use dillidb
 go
+exec sp_help tbl_just
 create table tbl_just
 (
 sno int primary key identity(1,1),
@@ -63,9 +66,8 @@ name varchar(50),
 age int,
 gender char(1)
 )
-exec sp_help tbl_just
 select * from dbo.tbl_just
-
+go
 create proc taskIns_sp
 @name varchar(50),
 @gender char(1)
@@ -73,16 +75,16 @@ as
 insert into tbl_just(name,gender)
 values(@name,@gender)
 
-taskIns_sp 'dilli','M'
-
-create proc taskup_sp
-@sno int,
+taskIns_sp 'gayathri','F'
+go
+create proc taskUp_sp
 @age int,
+@sno int
 as
 update tbl_just set age=@age where sno = @sno
 
-taskup_sp 19,1
-
+taskup_sp 20,3
+go
 create proc taskDel_sp
 @sno int
 as
@@ -96,27 +98,27 @@ use AdventureWorks2019
 go
 select name, DaysToManufacture
 from Production.Product 
-where name = ('blade')(select b.Name,b.DaysToManufacture from Production.Product b
-where b.DaysToManufacture like('1'))
+where DaysToManufacture = (select DaysToManufacture from Production.Product where Name = 'Blade')
 
 --Task6
 
 select Name
 from Production.Product
-where  name like '%all%'  or name like '%any%' or name like '%some%'
+where  Name like '%ALL%'  or name like '%ANY%' or name like '%soME%'
 
-select max(Weight) as Maxweight,ProductModelID
+select ProductModelID,weight
 from  Production.Product
-GRoup By ProductModelID
+where weight in (select max(weight) from Production.Product GRoup By ProductModelID)
 
 --Task7
 
-select FirstName,LastName
-from Person.Person
-select name
-from Sales.SalesTerritory sst
-join Sales.SalesPerson sp
-on sp.TerritoryID=sst.TerritoryID
-select max(SalesLastYear),Name
-from Sales.SalesTerritory
-group by Name
+use  AdventureWorks2019
+go
+
+select FirstName,LastName,ster.name,CountryRegionCode
+from Person.Person per
+join Sales.SalesPerson sper
+on per.BusinessEntityID = sper.BusinessEntityID
+join Sales.SalesTerritory ster
+on sper.TerritoryID= ster.TerritoryID
+where sper.salesYTD in (select MAX(SalesYTD) from Sales.SalesPerson group by TerritoryID)
