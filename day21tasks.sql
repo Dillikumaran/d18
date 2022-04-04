a3843677564e9f3db6cec65635cfe1661da617e3
Dillikumaran <101106085+Dillikumaran@users.noreply.github.com>
2022-04-04 20:17:36 +0530

    Update day21tasks.sql

--- day21tasks.sql
+++ day21tasks.sql
@@ -19,43 +19,52 @@ SELECT LEFT('Dilli', 3) AS ExtractString;
 select LEN('Dillikumaran')
 --USING REVERSE()
 select REVERSE('TeneT')
---Task 2                                                               
-create function fn_RupeesC(@amount as Nvarchar(100))
-returns Nvarchar(100)
+
+--Task 2 
+
+create function fn_RupeesC(@amount as Nvarchar(10))
+returns Nvarchar(10)
 as 
 begin
 return (N'₹'+@amount)
 end
 select BusinessEntityID,dbo.fn_RupeesC(rate) as Rupees 
 from HumanResources.EmployeePayHistory
+
 --Task3
+
 create table tbl_student
 (
-Sno int,
+Sno int primary key identity (1,1),
 StuName varchar(20),
-StuAge int
+StuAge int,
+MobileNO char(10),
+Gender char(1)
 )
-alter table tbl_student
-add Gender char(1)
-select * from tbl_student
-alter table tbl_student
-add MobileNO int
-insert into tbl_student values(1,'Dilli',19,'M',9600584521)
-alter table tbl_student
-alter column MobileNO char(10)
+insert into tbl_student values('Dilli',19,'M',9600584521)
+
 create view task3
 as
-select [Sno],[StuName],[StuAge]
-from [dbo].[tbl_student]
+select Sno,StuName,StuAge
+from dbo.tbl_student
 go
 select *from task3
-insert into tbl_student values(2,'selena',19,'F',9945215421)
+insert into task3 values('selena',19,'F',9945215421)
+
 --Task4
+
 use dillidb
 go
+create table tbl_just
+(
+sno int primary key identity(1,1),
+name varchar(50),
+age int,
+gender char(1)
+)
 exec sp_help tbl_just
 select * from dbo.tbl_just
-go
+
 create proc taskIns_sp
 @name varchar(50),
 @gender char(1)
@@ -64,36 +73,43 @@ insert into tbl_just(name,gender)
 values(@name,@gender)
 
 taskIns_sp 'dilli','M'
+
 create proc taskup_sp
 @sno int,
 @age int,
-@name varchar(50)
 as
-update tbl_just set sno=@sno,age=@age where name = @name
+update tbl_just set age=@age where sno = @sno
 
-task1_sp 1,19,'dilli'
+taskup_sp 19,1
+
 create proc taskDel_sp
 @sno int
 as
 delete from tbl_just where sno = @sno
 
 taskDel_sp 1
+
 --Task5
+
 use AdventureWorks2019
 go
 select name, DaysToManufacture
 from Production.Product 
 where name = ('blade')(select b.Name,b.DaysToManufacture from Production.Product b
 where b.DaysToManufacture like('1'))
+
 --Task6
-select[Name]
-from[Production].[Product]
+
+select Name
+from Production.Product
 where  name like '%all%'  or name like '%any%' or name like '%some%'
 
-select max([Weight]) as Maxweight,[ProductModelID]
-from  [Production].[Product]
-GRoup By [ProductModelID]
+select max(Weight) as Maxweight,ProductModelID
+from  Production.Product
+GRoup By ProductModelID
+
 --Task7
+
 select FirstName,LastName
 from Person.Person
 select name
